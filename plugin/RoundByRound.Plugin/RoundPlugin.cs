90a4143bca48328ab571394e408662a5a535bbf7
DgoNeves <kidix7@gmail.com>
2021-09-16 23:17:38 +0100

    restructed to have both projects

---
+++ plugin/RoundByRound.Plugin/RoundPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundByRound.Plugin
+{
+    public class RoundPlugin
+    {
+        public void LiveEvent(string jsonEvent, Action<object> callback)
+        {
+            if (callback == null)
+            {
+                return;
+            }
+
+            Task.Run(() => {
+                callback("From C#:" + jsonEvent);
+            });
+        }
+    }
+}
